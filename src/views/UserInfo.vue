08b80f3a6030c9ac0a00f7cf596e6e6a2f51a8de
陈龙 <dimaasstring@outlook.com>
2023-04-11 15:37:33 +0800

    dev_Chen_3.26

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -19,23 +19,34 @@
             <el-container>
                 <el-main>
                     <div style="">
-                        <el-input style="width: 200px; margin-left: 20px;" v-model="searchText"
-                                  placeholder="请输入用户签到码"></el-input>
+                        <span style="font-size: 14px;margin-left: 20px;">签到码：</span>
+                        <el-input style="width: 200px; margin-left: 10px;" v-model="searchText"
+                                  placeholder="请输入用户签到码">
+                        </el-input>
+                        <span style="font-size: 14px;margin-left: 10px;">签到状态：</span>
+                        <el-select v-model="selectType" placeholder="请选择" style="margin-left: 10px;width: 100px;">
+                            <el-option
+                                v-for="item in options"
+                                :key="item.value"
+                                :label="item.label"
+                                :value="item.value">
+                            </el-option>
+                        </el-select>
                         <el-button style="margin-left: 20px;" type="primary" icon="el-icon-search" @click="search">
                             搜索
                         </el-button>
-<!--                        <el-button style="margin-left: 20px;" type="primary" icon="el-icon-search" @click="search2">-->
-<!--                            搜索2-->
-<!--                        </el-button>-->
+                        <!--                        <el-button style="margin-left: 20px;" type="primary" icon="el-icon-search" @click="search2">-->
+                        <!--                            搜索2-->
+                        <!--                        </el-button>-->
                     </div>
                     <div style="height: 84%">
-                        <el-table :data="users"  height="100%" style="width: 100%;  margin: 20px;"
+                        <el-table :data="users" height="100%" style="width: 100%;  margin: 20px;"
                                   :header-cell-style="{textAlign: 'center'}" :cell-style="{ textAlign: 'center' }">
                             <el-table-column label="序号" width="50px;">
                                 <template slot-scope="scope">{{ scope.$index + 1 }}</template>
                             </el-table-column>
                             <el-table-column label="名称" prop="name"></el-table-column>
-                            <el-table-column label="昵称" prop="nickname"></el-table-column>
+                            <el-table-column label="签到码" prop="code"></el-table-column>
                             <el-table-column label="性别" prop="gender">
                                 <template slot-scope="scope">
                                     <el-tag :type="scope.row.gender == 1 ? 'success': 'primary'">
@@ -43,13 +54,20 @@
                                     </el-tag>
                                 </template>
                             </el-table-column>
-                            <el-table-column label="签到码" prop="code"></el-table-column>
                             <el-table-column label="电话" prop="phone"></el-table-column>
                             <el-table-column label="邮箱" prop="email"></el-table-column>
                             <el-table-column label="最近登录时间" prop="lastLoginTime"></el-table-column>
+                            <el-table-column label="签到情况" prop="signin">
+                                <template slot-scope="scope">
+                                    <el-tag :type="scope.row.signin == 1 ? 'success': 'primary'">
+                                        {{ scope.row.signin == 1 ? '签到成功' : '暂未签到' }}
+                                    </el-tag>
+                                </template>
+                            </el-table-column>
                             <el-table-column label="操作" width="200">
                                 <template slot-scope="scope">
                                     <el-popconfirm
+                                            v-if="scope.row.signin == 0"
                                             style="margin-left: 10px;"
                                             title="是否为当前用户进行签到？"
                                             confirm-button-text='确认'
@@ -57,8 +75,22 @@
                                             icon="el-icon-info"
                                             @confirm="editUser(scope.row)"
                                     >
-                                        <el-button type="primary"  icon="el-icon-edit"
+                                        <el-button  type="primary" icon="el-icon-edit"
                                                    slot="reference">场地签到
+                                        </el-button>
+                                    </el-popconfirm>
+
+                                    <el-popconfirm
+                                        v-if="scope.row.signin == 1"
+                                        style="margin-left: 10px;"
+                                        title="是否为当前用户取消签到状态？"
+                                        confirm-button-text='确认'
+                                        cancel-button-text='取消'
+                                        icon="el-icon-info"
+                                        @confirm="editUser(scope.row)"
+                                    >
+                                        <el-button  type="danger" icon="el-icon-edit"
+                                                   slot="reference">取消签到
                                         </el-button>
                                     </el-popconfirm>
                                 </template>
@@ -86,6 +118,17 @@ import axios from "axios";
 export default {
     data() {
         return {
+            options: [{
+                value: 'all',
+                label: '(全部)'
+            }, {
+                value: 'signing',
+                label: '已签到'
+            }, {
+                value: 'notsigning',
+                label: '未签到'
+            }],
+            selectType: 'all',
             tempUser: {
                 name: 'admin',
                 gender: 0,
@@ -98,24 +141,6 @@ export default {
             pageSize: 10,
             totalNum: 20,
             users: [
-                {
-                    name: '陈龙',
-                    nickname: '珅式',
-                    gender: 0,
-                    code: '1008',
-                    phone: '13800138000',
-                    email: 'ShenSHI@test.com',
-                    lastLoginTime: '2021-08-01 10:00:00',
-                },
-                {
-                    name: '陈龙',
-                    nickname: '珅式',
-                    gender: 1,
-                    code: '1006',
-                    phone: '13800138000',
-                    email: 'ShenSHI@test.com',
-                    lastLoginTime: '2021-08-01 10:00:00',
-                },
             ],
         };
     },
@@ -133,7 +158,7 @@ export default {
             console.log(`当前页: ${val}`);
         },
         getList() {
-            const data = {pageid: this.currentPage, pagenumber: this.pageSize};
+            const data = {pageid: this.currentPage, pagenumber: this.pageSize, selectType: this.selectType};
             const jsonStr = JSON.stringify(data);
             axios({
                     method: "post",
@@ -143,7 +168,7 @@ export default {
             ).then((res) => {
                 const dataList = res.data.data;
                 this.totalNum = res.data.totalNumbers;
-                    this.users = [];
+                this.users = [];
                 for (let i = 0; i < dataList.length; i++) {
                     const tempItem = dataList[i];
                     const pushItem = {
@@ -155,6 +180,7 @@ export default {
                         phone: tempItem.phone,
                         email: tempItem.email,
                         lastLoginTime: tempItem.Logintime.slice(0, 10) + " " + tempItem.Logintime.slice(11, 19),
+                        signin: tempItem.signin,
                     }
                     this.users.push(pushItem);
                 }
@@ -176,6 +202,31 @@ export default {
         editUser(user) {
             // 编辑员工操作
             console.log(user)
+            const data = {id: user.id};
+            const jsonStr = JSON.stringify(data);
+            if (user.signin == 1){
+                axios({
+                        method: "post",
+                        url: "http://192.168.31.82:9000/order/UpdateSigninBy0",
+                        data: jsonStr
+                    }
+                ).then((res) => {
+                    this.$toast.success('签到状态修改成功')
+                    this.searchText = ''
+                    this.getList();
+                })
+            } else {
+                axios({
+                        method: "post",
+                        url: "http://192.168.31.82:9000/order/UpdateSigninBy1",
+                        data: jsonStr
+                    }
+                ).then((res) => {
+                    this.$toast.success('签到状态修改成功')
+                    this.searchText = ''
+                    this.getList();
+                })
+            }
         },
         deleteUser(user) {
             // 删除员工操作
@@ -193,7 +244,7 @@ export default {
             // ).then((res) => {
             //     console.log(res.data);
             // })
-            if (this.searchText == ''){
+            if (this.searchText == '') {
                 this.getList();
                 return;
             }
@@ -219,6 +270,7 @@ export default {
                         phone: tempItem.phone,
                         email: tempItem.email,
                         lastLoginTime: tempItem.Logintime.slice(0, 10) + " " + tempItem.Logintime.slice(11, 19),
+                        signin: tempItem.signin,
                     }
                     this.users.push(pushItem);
                 }
